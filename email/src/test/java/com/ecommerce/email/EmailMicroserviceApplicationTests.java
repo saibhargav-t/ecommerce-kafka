eeec93301b71ff0ecc8107e297342175d9c1a68b
Bhargav-local <bhargavindian@outlook.com>
2025-11-29 01:05:25 +0530

    Created and extracted email from spring initializr

---
+++ email/src/test/java/com/ecommerce/email/EmailMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
